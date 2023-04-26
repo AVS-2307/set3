eeb3ad283dbcb62118a1817cb16ef9506317068c
Andrey Shestakov <andrey230781.an@gmail.com>
2023-04-26 17:47:42 +0300

    add tests for character.js

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -11,3 +11,24 @@ test('Получим объект Character', () => {
     };
     expect(description).toEqual(result);
 });
+
+test('Получим ошибку имени', () => {
+    const description = () => new Character('Bravo111111', 'Bowman');
+    expect(description).toThrow('Name length should be between 2 and 10 letters');
+  });
+  
+  test('Получим ошибку типа имени', () => {
+      const description = () => new Character(15, 'nsw');
+      expect(description).toThrow('Name should be string');
+  });
+  
+  test('Получим ошибку типа', () => {
+      const description = () => new Character('Nick', 15);
+      expect(description).toThrow('Type should be string');
+  });
+  
+  test('Получим ошибку типа2', () => {
+      const description = () => new Character('Nick', '15');
+      expect(description).toThrow('Type should be one of this: Bowman, Daemon, Magician, Swordsman, Undead, Zombie');
+  });
+  
